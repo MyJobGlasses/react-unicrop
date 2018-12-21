7e56beea6de89c620012b4f34ded1fd0588d315a
Dimitri DOMEY <dimitri@myjobglasses.com>
2018-12-21 11:28:33 +0100

    Handle bounds

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ export default class Cropper extends Component {
   constructor(props) {
     super(props)
     this.onDragStop = this.onDragStop.bind(this)
+    this.onImageLoaded = this.onImageLoaded.bind(this)
     this.state = {
       wrapperHeight: 0,
       wrapperWidth: 0,
@@ -15,8 +16,17 @@ export default class Cropper extends Component {
       holePositionY: 0,
       picturePositionX: 0,
       picturePositionY: 0,
+      pictureHeight: 0,
+      pictureWidth: 0,
+      bounds: {
+        top: 0,
+        left: 0,
+        bottom: 10000,
+        right: 10000,
+      },
     }
     this.wrapperRef = React.createRef()
+    this.pictureRef = React.createRef()
   }
 
   componentDidMount() {
@@ -31,10 +41,63 @@ export default class Cropper extends Component {
     })
   }
 
-  onDragStop(_, data) {
+  componentDidUpdate({ holeSize }, { holePositionX, holePositionY, pictureHeight, pictureWidth }) {
+    if (
+      this.props.holeSize !== holeSize ||
+      this.state.holePositionX !== holePositionX ||
+      this.state.holePositionY !== holePositionY ||
+      this.state.pictureHeight !== pictureHeight ||
+      this.state.pictureWidth !== pictureWidth
+    ) {
+      this._calculateDraggableBounds()
+    }
+  }
+
+  /**
+   * Retrieve picture size when loaded
+   */
+  onImageLoaded() {
+    const pictureHeight = this.pictureRef.current.clientHeight
+    const pictureWidth = this.pictureRef.current.clientWidth
+    this.setState({
+      pictureHeight,
+      pictureWidth,
+    })
+  }
+
+  /**
+   * Calculate draggable bounds
+   * Use maximum draggable x / y
+   */
+  _calculateDraggableBounds() {
+    const {
+      holePositionX,
+      holePositionY,
+      pictureHeight,
+      pictureWidth,
+    } = this.state
+    const {
+      holeSize,
+    } = this.props
+    this.setState({
+      bounds: {
+        top: holePositionY + holeSize - pictureHeight,
+        left: holePositionX + holeSize - pictureWidth,
+        bottom: holePositionY,
+        right: holePositionX,
+      },
+    })
+  }
+
+  /**
+   * Calculate position on picture (with hole offset)
+   * Call onChange props function to inform parent component
+   * @param {Object} event - event data
+   * @param {Object} data - draggable data
+   */
+  onDragStop(_, { x, y }) {
     const { holePositionX, holePositionY } = this.state
     const { holeSize } = this.props
-    const { x, y } = data
     const picturePositionX = holePositionX - x
     const picturePositionY = holePositionY - y
     this.setState({
@@ -54,6 +117,7 @@ export default class Cropper extends Component {
       src,
       holeSize,
     } = this.props
+    const { bounds } = this.state
     return (
       <div>
         <div
@@ -62,8 +126,15 @@ export default class Cropper extends Component {
         >
           <Draggable
             onStop={this.onDragStop}
+            bounds={bounds}
           >
-            <img src={src} alt='random' draggable={false} />
+            <img
+              ref={this.pictureRef}
+              src={src}
+              alt='Image to crop'
+              draggable={false}
+              onLoad={this.onImageLoaded}
+            />
           </Draggable>
           <div
             className={styles.hole}
